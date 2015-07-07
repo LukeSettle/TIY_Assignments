b230d21ecad637efc236282f50ebdfec8b212b30
Luke Settle <lukewestonsettle@gmail.com>
2015-07-06 19:30:59 -0500

    It works with me testing it, but wont pass the minitest...

--- Week 2/Ships.rb
+++ Week 2/Ships.rb
@@ -18,29 +18,20 @@ class Ship
 	end
 
 	def covers? a, b
-	   x = @x
-	   y = @y
-	   (@size - 1).times do
-	     if @axis
-	       	x += 1
-	     else
-	       	y += 1
-	     end
-	     return true if x == a and y == b
-	   end
-	   false
+	   	x = @x
+	  	y = @y
+	  	if x == a and y == b
+	  		return true
+	  	else
+	    	(@size - 1).times do
+	    		if @axis
+	    	   		x += 1
+	    	 	else
+	    	   		y += 1
+	    	 	end
+	    	 	return true if x == a and y == b
+	    	end
+	   		false
+	   	end
 	 end
-
-		# if @axis == true
-		# 	if left > self.length || right != @y || left == 1
-		# 		false
-		# 	else
-		# 		true
-		# 	end	
-		# elsif @axis == false
-		# 	if right > self.length || left != @x || right == 1
-		# 		false
-		# 	else
-		# 		true
-		# 	end	
 end
--- Week 2/battleship.rb
+++ Week 2/battleship.rb
@@ -3,26 +3,3 @@ require_relative "HumanPlayer"
 require_relative "ComputerPlayer"
 require_relative "Ships"
 require "pry"
-ship1 = Ship.new(4)
-ship1.place(2, 1, true)
-ship1.covers?(2, 1)
-ship1.covers?(3, 1)
-ship1.covers?(4, 1)
-binding.pry
-ship1.covers?(5, 1)
-ship1.covers?(1, 1)
-ship1.covers?(6, 1)
-ship1.covers?(4, 2)
-
-#main file
-
-ship2 = Ship.new(4)
-ship2.place(2, 2, false)
-ship2.covers?(2, 1)
-ship2.covers?(2, 2)
-ship2.covers?(2, 3)
-ship2.covers?(2, 4)
-binding.pry
-ship2.covers?(2, 5)
-ship2.covers?(2, 6)
-ship2.covers?(3, 2)
