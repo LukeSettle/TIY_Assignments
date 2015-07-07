63a7e93ceb5b1b9923fa6692c4765284767e42ca
Luke Settle <lukewestonsettle@gmail.com>
2015-07-06 19:05:22 -0500

    added ships class

--- Week 2/Ships.rb
+++ Week 2/Ships.rb
@@ -1,5 +1,7 @@
+require "pry"
+
 class Ship
-	attr_reader :size
+	attr_reader :size, :x, :y, :axis
 
 	def initialize size
 		@size = size
@@ -9,12 +11,36 @@ class Ship
 		@size = size
 	end
 
-	def place x y
+	def place x, y, axis
 		@x = x
 		@y = y
+		@axis = axis
 	end
 
-	def covers?
+	def covers? a, b
+	   x = @x
+	   y = @y
+	   (@size - 1).times do
+	     if @axis
+	       	x += 1
+	     else
+	       	y += 1
+	     end
+	     return true if x == a and y == b
+	   end
+	   false
+	 end
 
-	end
+		# if @axis == true
+		# 	if left > self.length || right != @y || left == 1
+		# 		false
+		# 	else
+		# 		true
+		# 	end	
+		# elsif @axis == false
+		# 	if right > self.length || left != @x || right == 1
+		# 		false
+		# 	else
+		# 		true
+		# 	end	
 end
--- Week 2/battleship.rb
+++ Week 2/battleship.rb
@@ -2,9 +2,27 @@ require_relative "Player"
 require_relative "HumanPlayer"
 require_relative "ComputerPlayer"
 require_relative "Ships"
+require "pry"
+ship1 = Ship.new(4)
+ship1.place(2, 1, true)
+ship1.covers?(2, 1)
+ship1.covers?(3, 1)
+ship1.covers?(4, 1)
+binding.pry
+ship1.covers?(5, 1)
+ship1.covers?(1, 1)
+ship1.covers?(6, 1)
+ship1.covers?(4, 2)
 
-Player.new "human"
-ComputerPlayer.new
-human = HumanPlayer.new "luke2"
 #main file
-puts human.name
+
+ship2 = Ship.new(4)
+ship2.place(2, 2, false)
+ship2.covers?(2, 1)
+ship2.covers?(2, 2)
+ship2.covers?(2, 3)
+ship2.covers?(2, 4)
+binding.pry
+ship2.covers?(2, 5)
+ship2.covers?(2, 6)
+ship2.covers?(3, 2)
